46da0c6a40886e0f79f168c40251a800dcbadc55
root <root@localhost>
2025-01-23 14:16:02 +0000

    Revert "mt6768: drivers: mediatek/gpu: Add GED_SKI support"
    This reverts commit 373bfcd1819e462eeb5bf8070cf13b793e95362b.

--- drivers/gpu/mediatek/ged/src/ged_main.c
+++ drivers/gpu/mediatek/ged/src/ged_main.c
@@ -40,9 +40,6 @@
 #include "ged_kpi.h"
 #include "ged_ge.h"
 #include "ged_gpu_tuner.h"
-#ifdef GED_SKI_SUPPORT
-#include "ged_ski.h"
-#endif
 
 /**
  * ===============================================
@@ -479,14 +476,6 @@ static int ged_pdrv_probe(struct platform_device *pdev)
 		goto ERROR;
 	}
 
-#ifdef GED_SKI_SUPPORT
-	err = ged_ski_init();
-	if (unlikely(err != GED_OK)) {
-		GED_LOGE("ged: failed to init ski!\n");
-		goto ERROR;
-	}
-#endif
-
 #ifndef GED_BUFFER_LOG_DISABLE
 	ghLogBuf_GPU = ged_log_buf_alloc(512, 128 * 512,
 		GED_LOG_BUF_TYPE_RINGBUFFER, "GPU_FENCE", NULL);
@@ -581,10 +570,6 @@ static void ged_exit(void)
 	ged_log_buf_free(ghLogBuf_GPU);
 	ghLogBuf_GPU = 0;
 #endif /* GED_BUFFER_LOG_DISABLE */
-
-#ifdef GED_SKI_SUPPORT
-	ged_ski_exit();
-#endif
 
 	ged_gpu_tuner_exit();
 
